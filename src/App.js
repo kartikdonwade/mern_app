e2db5f9bd08ef77181c3fadbee5595d62e2543b2
Kartik Donwade <kartikdonwade45@gmail.com>
2024-04-06 23:01:20 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Home from './Screens/Home';
+
+function App() {
+  return (
+   <div>
+<Home></Home>
+   </div>
+  );
+}
+
+export default App;
---
+++ src/Screens/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+import Footer from '../Components/Footer'
+
+export default function Home() {
+  return (
+    <div>
+        <div><Navbar/></div>
+   <div>Body</div>
+   <div><Footer/></div>
+    </div>
+ 
+  )
+}
